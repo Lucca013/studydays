588c45a01b5d3d7d1e9ee224d4856ae098473314
Lucca <luccacenteno013@gmail.com>
2026-09-15 07:42:23 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/studydays/studydays/StudydaysApplication.java
@@ -0,0 +1,13 @@
+package com.studydays.studydays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudydaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudydaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studydays/studydays/StudydaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studydays.studydays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudydaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
