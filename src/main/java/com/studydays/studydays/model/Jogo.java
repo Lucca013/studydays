03a5b9474b2463ff0dd4ec7645bc4125684abd52
Lucca <luccacenteno013@gmail.com>
2026-09-21 16:21:27 -0300

    adicionei as classes Jogo e Ranking

---
+++ src/main/java/com/studydays/studydays/model/Jogo.java
@@ -0,0 +1,33 @@
+package com.studydays.studydays.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table 
+@Getter 
+@Setter 
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class Jogo {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String status;
+
+    @Column (nullable = false)
+    private Usuario jogador;
+
+    // futuramente, para implementar a lógica do jogo, também será adicionado:
+    // uma referência a classe "Evento" e "Progresso" 
+}
